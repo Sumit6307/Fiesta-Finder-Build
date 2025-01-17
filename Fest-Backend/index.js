8757e40d5fccc5fe447f714f725c0345a6e78a8b
srajan gupta <145918331+creation22@users.noreply.github.com>
2025-01-17 09:52:17 +0000

    dry run

---
+++ Fest-Backend/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express ()
+const port = 4000
+
+app.get('/', (req ,res) => {
+    res.send("Hello world ")
+})
+app.listen(process.env.PORT , () => {
+    console.log(`example app is listening on port ${port}`)
+})
