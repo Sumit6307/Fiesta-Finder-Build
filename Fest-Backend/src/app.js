615004ec6b8542a117a7cda71e9f1d91f90a8c0f
srajan gupta <145918331+creation22@users.noreply.github.com>
2025-01-18 07:25:24 +0000

    express not running

--- Fest-Backend/src/app.js
+++ Fest-Backend/src/app.js
@@ -14,5 +14,5 @@ app.use(cookieParser())
 // routes 
 import userRouter from './routes/user.routes.js'
 // routes declaration 
-app.use("/api/v1/users",userRouter)
+app.use("/fiesta/users",userRouter)
 export {app}
